5cf36fae9f95a997aeece3cacaec1a7823caf4d8
Dwiky Meidy Lasmana <dwikylasmana@gmail.com>
2021-05-31 12:18:50 +0700

    Ver 0.3

--- about.php
+++ about.php
@@ -3,6 +3,8 @@
         <title></title>
         <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     </head>
 
@@ -25,5 +27,63 @@
                 </li>
             </ul>
         </nav>
+
+        <div class="jumbotron" style="text-align: center;">
+            <h1 class="display-4">Kontak Kami</h1>
+            <p class="lead">Hubungi Kami.</p>
+            <hr class="my-4">
+        </div>
+
+        <div class="card-deck" style="margin: 100px 100px 100px 450px;">
+            <div class="card">
+                <img src="assets\images\Fazar.png" alt="Satria" style="height:100%">
+                <h2>Fazar Zulham Ibrahim</h2>
+                <p class="title">065118118</p>
+                <h3>Universitas Pakuan</h3>
+                <a href="#"><i class="fa fa-whatsapp"> 0858-1051-0492</i></a>
+                <a href="#"><i class="fa fa-facebook"> N-Pass Distributor Man </i></a>
+                <p><button>Kontak</button></p>
+            </div>
+            <div class="card">
+                <img src="assets\images\Candra.png" alt="Satria" style="height:100%">
+                <h2>Satria Candra Winata</h2>
+                <p class="title">065118118</p>
+                <h3>Universitas Pakuan</h3>
+                <a href="#"><i class="fa fa-whatsapp"> 0821-2009-2164</i></a>
+                <a href="#"><i class="fa fa-facebook"> xXLoliHunter_69Xx </i></a>
+                <p><button>Kontak</button></p>
+            </div>
+            <div class="card">
+                <img src="assets\images\Dwiky.PNG" alt="Dwiky" style="height:900%">
+                <h2>Dwiky Meidy Lasmana</h2>
+                <p class="title">065118085</p>
+                <h3>Universitas Pakuan</h3>
+                <a href="#"><i class="fa fa-whatsapp"> 0838-1919-2260</i></a>
+                <a href="#"><i class="fa fa-facebook"> IXION </i></a>
+                <p><button>Kontak</button></p>
+            </div>
+        </div>
+        <div class="card-deck" style="margin: 100px 100px 100px 650px;">
+            <div class="card">
+                <img src="assets\images\Kris.PNG" alt="Kris" style="height:100%">
+                <h2>Kristianto</h2>
+                <p class="title">065118089</p>
+                <h3>Universitas Pakuan</h3>
+                <a href="#"><i class="fa fa-whatsapp"> 0822-1054-0309</i></a>
+                <a href="#"><i class="fa fa-facebook"> Chinese Boy </i></a>
+                <p><button>Kontak</button></p>
+            </div>
+
+            <div class="card">
+                <img src="assets\images\Findi.PNG" alt="Findi" style="height:100%">
+                <h2>Findi Yandaris</h2>
+                <p class="title">065118104</p>
+                <h3>Universitas Pakuan</h3>
+                <a href="#"><i class="fa fa-whatsapp"> 0821-7775-2859</i></a>
+                <a href="#"><i class="fa fa-facebook"> Zulul </i></a>
+                <p><button>Kontak</button></p>
+            </div>
+
+        </div>
     </body>
 </html>
